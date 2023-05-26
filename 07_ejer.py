fcfc3452f9010a3011febc3c98f45414287e18f8
Javi <120780139+Jarv1er@users.noreply.github.com>
2023-05-26 23:21:49 +0200

    Update 07_ejer.py

--- 07_ejer.py
+++ 07_ejer.py
@@ -2,8 +2,8 @@
 
 print("Reste dos numeros")
 
-n1 = int(input("Escribe un numero: "))
-n2 = int(input("Escribe un numero: "))
+n1 = int(input("Escribe el primer numero: "))
+n2 = int(input("Escribe el segundo numero: "))
 result = n1 - n2
 
-print(f"{n1} menos {n2} es: {result}")
+print(f"{n1} - {n2} = {result}")
