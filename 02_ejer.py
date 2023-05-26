611a533f57fd6bdcde7a9ffc42fb5d024b56edbb
Javi <120780139+Jarv1er@users.noreply.github.com>
2023-05-26 23:20:26 +0200

    Update 02_ejer.py

--- 02_ejer.py
+++ 02_ejer.py
@@ -1,13 +1,12 @@
 # Software que permita sumar y restar (ambas opciones por ejecución) números enteros.
 
+print("Suma y resta dos numeros enteros")
+
 n1 = int(input("Ingresa el primer numero: "))
 
 n2 = int(input("Ingresa el segundo numero: "))
 
-op = input("¿Que operacion quieres realizar sumar o restar?: ")
+suma = n1 + n2
+resta = n1 - n2
 
-if op == "sumar":
-    print(n1 + n2)
-
-elif op == "restar":
-    print(n1 - n2)
+print("Suma: {} + {} = {}\nResta: {} - {} = {}".format(n1, n2, suma, n1, n2, resta))
