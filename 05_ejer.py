d457aed8813e43f631f31c48b7ea95499f852862
Javi <120780139+Jarv1er@users.noreply.github.com>
2023-05-27 12:50:20 +0200

    Update 05_ejer.py

--- 05_ejer.py
+++ 05_ejer.py
@@ -1,7 +1,10 @@
 # Software que permita obtener el número de cifras que contiene un número entero
 
-num = input("Escribe un numero: ")
+n = int(input("Escribe un numero: "))
+cifras = 0
 
-cifras = len(num)
+while n > 0:
+    n = n // 10
+    cifras += 1
 
-print(f"{num} Contiene {cifras} cifras" )
+print(f"El numero tiene {cifras} cifras")
